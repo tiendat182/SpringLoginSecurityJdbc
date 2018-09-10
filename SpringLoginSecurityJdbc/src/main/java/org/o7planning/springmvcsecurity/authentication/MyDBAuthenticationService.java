3d427007100cf7ab99029059aa70f993b2756c57
tiendat182 <tiendat.fet4@gmail.com>
2018-09-10 22:46:44 +0700

    test source tree

--- SpringLoginSecurityJdbc/src/main/java/org/o7planning/springmvcsecurity/authentication/MyDBAuthenticationService.java
+++ SpringLoginSecurityJdbc/src/main/java/org/o7planning/springmvcsecurity/authentication/MyDBAuthenticationService.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyDBAuthenticationService implements UserDetailsService {
+public class MyDBAuthenticationService implements UserDetailsService { 
 
 	@Autowired(required = true)
 	private UserInfoDAO userInfoDAO;
